86dc03b029797c0f1278cbb3fbc5ff8540273e58
Jan Wild <jan.wild@hotmail.com>
2016-02-18 18:29:47 +0100

    Adding my triviaMVA without routing

---
+++ Work/TriviaMVAWjut/TriviaMVA/app.py
@@ -0,0 +1,32 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+
+@app.route('/')
+def hello():
+    """Renders a sample page."""
+    return """<html>
+                <head>
+                    <title>Hello world</title>
+                    <body>
+                        <h1>Hello Johny</h1>
+                    </body>
+                </head>
+            </html>"""
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
